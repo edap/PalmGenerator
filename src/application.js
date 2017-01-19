af6e1cb9adb9f23250acc458bc42940af219ea72
edapx <lastexxit@gmail.com>
2017-01-19 09:07:06 +0100

    add movement

--- src/application.js
+++ src/application.js
@@ -22,6 +22,8 @@ var params = {
     num: 100,
     rotate_flower: false,
     extrude_flower: false,
+    amplitude: 0.1,
+    inc_bla: 0.5,
     pos_x: 0,
     pos_y: 0,
     pos_z: 0
@@ -33,7 +35,9 @@ gui.add(params, "pos_y").min(-10).max(10).step(1);
 gui.add(params, "pos_z").min(0).max(70).step(1);
 gui.add(params, "num").min(1).max(400).step(1);
 gui.add(params, "angle").min(130).max(140).step(0.2);
-gui.add(params, "spread").min(1).max(70).step(1);
+gui.add(params, "spread").min(0).max(20).step(0.1);
+gui.add(params, "amplitude").min(0).max(10).step(0.1);
+gui.add(params, "inc_bla").min(0.1).max(1).step(0.1);
 gui.add(params, "rotate_flower");
 gui.add(params, "extrude_flower");
 
@@ -50,9 +54,9 @@ this.controls = new OrbitControls(camera, renderer.domElement);
 
 
 //light
-var light = new THREE.PointLight( 0xff0000, 1, 100 );
-light.position.set( 50, 50, 50 );
-scene.add( light );
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(1,0,1).normalize();
+scene.add(light);
 
 //populateFlower();
 
@@ -64,13 +68,19 @@ window.addEventListener('resize', function() {
     camera.updateProjectionMatrix();
 });
 
+var bla =0.02;
 function populateFlower() {
     for (var i = 0; i< params.num; i++) {
         let coord = phyllotaxis(i, params.angle, params.spread);
         let sphere = new THREE.Mesh(geometry, material);
         sphere.position.set(coord.x, coord.y, 0);
         if(params.extrude_flower){
-            sphere.position.z = i * -.05;
+            //sphere.position.z = i * -.05;
+            var z = Math.sin(bla) * params.amplitude;
+            console.log(z);
+            bla += params.inc_bla;
+            sphere.position.z = z;
+            console.log(z);
 				    sphere.rotateY( (90 + 40 + i * 100/params.num ) * Math.PI/180.0 );
         }
         objects.push(sphere);
