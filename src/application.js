5ed6cff0aa7738984ec43a0ed88d0b1eec61ebf5
edapx <lastexxit@gmail.com>
2017-01-22 16:55:37 +0100

    WIP refactoring app

--- src/application.js
+++ src/application.js
@@ -1,51 +1,13 @@
 /* eslint-env browser */
 import * as THREE from 'three';
-import Cube from './cube.js';
-import DAT from 'dat-gui';
+import Gui from './gui.js';
 import {phyllotaxisSimple, phyllotaxisSphere} from './phillotaxis.js';
+import CollectionGeometries from './geometries.js';
+import CollectionMaterials from './materials.js';
 
-//geometries
-const widthSegments = 32;
-const heightSegments = 32;
-const radius = 5;
-const geometries = {
-    "sphere": new THREE.SphereGeometry(radius, widthSegments, heightSegments),
-    "box": new THREE.BoxGeometry( radius, radius, radius, 4, 4, 4 )
-};
-
-var material = new THREE.MeshStandardMaterial( {color: 0x00ff00} );
-
-
-var objects = [];
-var flower = new THREE.Group();
 const scene = new THREE.Scene();
 const OrbitControls = require('three-orbit-controls')(THREE);
-const gui = new DAT.GUI();
-var params = {
-    geometry: "sphere",
-    angle: 137.5,
-    angle_b: 137.5,
-    spread: 10,
-    num: 100,
-    rotate_flower: false,
-    extrude_2Dflower: false,
-    amplitude: 0.1,
-    spherical:false,
-    inc_bla: 0.5
-};
-
-
-gui.add(params, "num").min(1).max(800).step(1);
-gui.add(params, "geometry", ["sphere", "box"]);
-gui.add(params, "angle").min(132.0).max(138.0).step(0.1);
-gui.add(params, "angle_b").min(137.3).max(137.6).step(0.1);
-gui.add(params, "spread").min(0).max(20).step(0.1);
-gui.add(params, "amplitude").min(0).max(10).step(0.1);
-gui.add(params, "inc_bla").min(0.1).max(1).step(0.1);
-gui.add(params, "rotate_flower");
-gui.add(params, "extrude_2Dflower");
-gui.add(params, "spherical");
-
+const gui = new Gui();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -53,13 +15,19 @@ document.body.appendChild(renderer.domElement);
 camera.position.z = 80;
 this.controls = new OrbitControls(camera, renderer.domElement);
 
+//scene
+const materials = new CollectionMaterials;
+const geometries = new CollectionGeometries;
+var objects = [];
+var flower = new THREE.Group();
 
 //light
 var light = new THREE.DirectionalLight(0xffffff);
 light.position.set(1,0,1).normalize();
 scene.add(light);
 
-//populateFlower();
+var axisHelper = new THREE.AxisHelper( 50 );
+scene.add( axisHelper );
 
 window.addEventListener('resize', function() {
     var WIDTH = window.innerWidth,
@@ -69,17 +37,17 @@ window.addEventListener('resize', function() {
     camera.updateProjectionMatrix();
 });
 
-function populateFlower(selected_geometry) {
-    for (var i = 0; i< params.num; i++) {
+function populateFlower(selected_geometry, selected_material) {
+    for (var i = 0; i< gui.params.num; i++) {
         let coord;
-        let object = new THREE.Mesh(selected_geometry, material);
-        if (params.spherical) {
-            coord = phyllotaxisSphere(i, params.angle, params.angle_b, params.spread, params.num);
+        let object = new THREE.Mesh(selected_geometry, selected_material);
+        if (gui.params.spherical) {
+            coord = phyllotaxisSphere(i, gui.params.angle, gui.params.angle_b, gui.params.spread, gui.params.num);
             object.position.set(coord.x, coord.y, coord.z);
         } else {
-            coord = phyllotaxisSimple(i, params.angle, params.spread, params.extrude_2Dflower);
+            coord = phyllotaxisSimple(i, gui.params.angle, gui.params.spread, gui.params.extrude_2Dflower);
             object.position.set(coord.x, coord.y, coord.z);
-				    object.rotateY( (90 + 40 + i * 100/params.num ) * Math.PI/180.0 );
+				    object.rotateY( (90 + 40 + i * 100/gui.params.num ) * -Math.PI/180.0 );
         }
 
         objects.push(object);
@@ -98,8 +66,8 @@ function resetFlower(){
 }
 
 function render(){
-    populateFlower(geometries[params.geometry]);
-    if(params.rotate_flower){
+    populateFlower(geometries[gui.params.geometry],materials[gui.params.material]);
+    if(gui.params.rotate_flower){
         flower.rotateZ( 0.0137);
     }
 	  requestAnimationFrame(render);
