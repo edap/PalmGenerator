7038b1d94c59b3e387aa5d9874777d109eb84f42
edapx <lastexxit@gmail.com>
2017-01-22 21:31:14 +0100

    add lights

--- src/application.js
+++ src/application.js
@@ -5,15 +5,18 @@ import {phyllotaxisSimple, phyllotaxisSphere} from './phillotaxis.js';
 import CollectionGeometries from './geometries.js';
 import CollectionMaterials from './materials.js';
 
+const gui = new Gui();
 const scene = new THREE.Scene();
 const OrbitControls = require('three-orbit-controls')(THREE);
-const gui = new Gui();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.style.margin =0;
 document.body.appendChild(renderer.domElement);
 camera.position.z = 80;
 this.controls = new OrbitControls(camera, renderer.domElement);
+
 
 //scene
 const materials = new CollectionMaterials;
@@ -21,13 +24,28 @@ const geometries = new CollectionGeometries;
 var objects = [];
 var flower = new THREE.Group();
 
-//light
-var light = new THREE.DirectionalLight(0xffffff);
-light.position.set(1,0,1).normalize();
-scene.add(light);
+//lights
+let ambientLight = new THREE.AmbientLight( 0x000000 );
+scene.add( ambientLight );
+gui.addScene(scene, ambientLight, renderer);
+gui.addMaterials(materials);
+
+let lights = [];
+lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+lights[ 0 ].position.set( 0, 200, 0 );
+lights[ 1 ].position.set( 100, 200, 100 );
+lights[ 2 ].position.set( - 100, - 200, - 100 );
+
+scene.add( lights[ 0 ] );
+scene.add( lights[ 1 ] );
+scene.add( lights[ 2 ] );
+
 
 var axisHelper = new THREE.AxisHelper( 50 );
-scene.add( axisHelper );
+//scene.add( axisHelper );
 
 window.addEventListener('resize', function() {
     var WIDTH = window.innerWidth,
