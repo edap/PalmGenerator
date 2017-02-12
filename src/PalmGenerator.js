712959b6234fe4a030344f285688a6149c2d7246
edapx <lastexxit@gmail.com>
2017-02-12 11:28:28 +0100

    add underscore to private methods

--- src/PalmGenerator.js
+++ src/PalmGenerator.js
@@ -8,34 +8,34 @@ export default class PalmGenerator{
         let objects;
         let result;
         let cleaned_options =
-            this.merge_and_validate_options(options, this.default_options());
+            this._merge_and_validate_options(options, this._default_options());
 
         if (cleaned_options.buffers){
-            let hash_vertex_info = this.getTotNumVertices(leaf_geometry,
+            let hash_vertex_info = this._getTotNumVertices(leaf_geometry,
                                                       trunk_geometry,
                                                       cleaned_options.num,
                                                       cleaned_options.foliage_start_at);
 
-            buffers = this.createBuffers(hash_vertex_info.tot_vertices);
-            objects = this.buildPalm(leaf_geometry,
+            buffers = this._createBuffers(hash_vertex_info.tot_vertices);
+            objects = this._buildPalm(leaf_geometry,
                                          trunk_geometry,
                                          cleaned_options);
-            geometry = this.mergeObjectsInOneGeometryAndFullfilBuffers(objects,
+            geometry = this._mergeObjectsInOneGeometryAndFullfilBuffers(objects,
                                                                            cleaned_options,
                                                                            hash_vertex_info,
                                                                            buffers);
             result =  { geometry:geometry, buffers: buffers };
-        }else{
-            objects = this.buildPalm(leaf_geometry,
+        } else {
+            objects = this._buildPalm(leaf_geometry,
                                          trunk_geometry,
                                          cleaned_options);
-            geometry = this.mergeObjectsInOneGeometry(objects, cleaned_options);
+            geometry = this._mergeObjectsInOneGeometry(objects, cleaned_options);
             result =  { geometry:geometry };
         }
         return result;
     }
 
-    default_options(){
+    _default_options(){
         return {
             spread: 0.2,
             angle: 137.5,
@@ -48,19 +48,19 @@ export default class PalmGenerator{
         };
     }
 
-    merge_and_validate_options(options, defaults){
+    _merge_and_validate_options(options, defaults){
         //TODO implement validations
         let opt = Object.assign(defaults, options);
         return opt;
     }
 
-    buildPalm(leaf_geometry, trunk_geometry, general_options){
+    _buildPalm(leaf_geometry, trunk_geometry, general_options){
         let material = new THREE.MeshBasicMaterial();
-        let objects = this.populatePalm(leaf_geometry, trunk_geometry, general_options, material);
+        let objects = this._populatePalm(leaf_geometry, trunk_geometry, general_options, material);
         return objects;
     }
 
-    populatePalm(foliage_geometry, trunk_geometry, options, material) {
+    _populatePalm(foliage_geometry, trunk_geometry, options, material) {
         let objs = [];
         let PItoDeg = (Math.PI/180.0);
         let angleInRadians = options.angle * PItoDeg;
@@ -74,7 +74,7 @@ export default class PalmGenerator{
             let coord = phyllotaxisConical(i, angleInRadians, options.spread, options.z_decrease);
             object.position.set(coord.x, coord.y, coord.z);
             if (isALeaf) {
-                this.transformIntoLeaf(object, i, angleInRadians, options);
+                this._transformIntoLeaf(object, i, angleInRadians, options);
             } else {
                 object.rotateZ( i* angleInRadians);
                 object.rotateY( (90 + options.angle_y + i * 100/options.num ) * -PItoDeg );
@@ -84,12 +84,12 @@ export default class PalmGenerator{
         return objs;
     }
 
-    transformIntoLeaf(object, iter, angleInRadians, options){
+    _transformIntoLeaf(object, iter, angleInRadians, options){
         let PItoDeg = (Math.PI/180.0);
-        //the scale ratio is a value between 0.001 and 1.
-        // It is 0.0001 for the first leaves, and 1 for the last ones
+        // The scale ratio is a value between 0.001 and 1.
+        // It is 0.0001 for the first leaves, and 1 for the last one
         let ratio = Math.abs(iter/options.foliage_start_at);
-        //this is to avaoid a scaleRatio of 0, that would cause a warning while scaling
+        // This is to avaoid a scaleRatio of 0, that would cause a warning while scaling
         // an object for 0
         let scaleRatio = ratio === 0 ? 0.001 : ratio;
         object.rotateZ( iter* angleInRadians);
@@ -97,14 +97,12 @@ export default class PalmGenerator{
         //object.rotateY( (yrot ) * -PItoDeg );
         let y_angle = options.angle_y * scaleRatio;
         object.rotateY( (options.starting_angle_y + y_angle + iter * 200/options.num ) * -PItoDeg );
-
-        // as they grow up, they become bigger
+        // as leaves grow up, they become bigger
         object.scale.set(5 * scaleRatio ,1 ,1);
         object.rotateZ(-(Math.PI/2));
     }
 
-    mergeObjectsInOneGeometryAndFullfilBuffers(objs, opt, vertex_info, buffers){
-        //let buffers = createBuffers(hash_vertex_info.tot_vertices);
+    _mergeObjectsInOneGeometryAndFullfilBuffers(objs, opt, vertex_info, buffers){
         let geometry = new THREE.Geometry();
         let current_pos = 0; // current position in the buffers
         for (let i = 0; i < objs.length; i++){
@@ -126,10 +124,9 @@ export default class PalmGenerator{
             mesh.updateMatrix();
             geometry.merge(mesh.geometry, mesh.matrix);
         }
-        //debugger;
         return geometry;
     }
-    mergeObjectsInOneGeometry(objects){
+    _mergeObjectsInOneGeometry(objects){
         let geometry = new THREE.Geometry();
         for (let i = 0; i < objects.length; i++){
             let mesh = objects[i];
@@ -139,15 +136,14 @@ export default class PalmGenerator{
         return geometry;
     }
 
-    // Here some functions used for the BUFFER_FEATURE, actually used. It is a WIP for calculating the buffers, but is not working
-    createBuffers(n_vert){
+    _createBuffers(n_vert){
         return {
             angle: new Float32Array(n_vert),
             isLeaf: new Float32Array(n_vert)
         };
     }
 
-    getTotNumVertices(foliage_geometry, trunk_geometry, tot_objects, foliage_start_at){
+    _getTotNumVertices(foliage_geometry, trunk_geometry, tot_objects, foliage_start_at){
         let adjusted_foliage_start_at = foliage_start_at + 1; //counting the 0 too
         let vertices_in_leaf = foliage_geometry.faces.length * 3;
         let vertices_in_trunk = trunk_geometry.faces.length * 3;
